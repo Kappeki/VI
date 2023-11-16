6783dc3c2c898fb3292be63a30c6d69444e95b6b
Kappeki <dusanpetrovic@elfak.rs>
2023-11-16 01:29:15 +0100

    Added BFS

--- Lab vezbe/Lab 3/main.py
+++ Lab vezbe/Lab 3/main.py
@@ -2,6 +2,40 @@
 import queue
 
 
+def bfs(graph, start, end):
+    if start == end:
+        return [start]
+
+    queue_nodes = queue.Queue(len(graph))
+    visited = set()
+    prev_nodes = dict()
+    prev_nodes[start] = None
+    visited.add(start)
+    queue_nodes.put(start)
+    found_dest = False
+
+    while (not found_dest) and (not queue_nodes.empty()):
+        node = queue_nodes.get()
+        for dest in graph[node]:
+            if dest not in visited:
+                prev_nodes[dest] = node
+                if dest is end:
+                    found_dest = True
+                    break
+                visited.add(dest)
+                queue_nodes.put(dest)
+
+    path = []
+    if found_dest:
+        path.append(end)
+        prev = prev_nodes[end]
+        while prev is not None:
+            path.append(prev)
+            prev = prev_nodes[prev]
+        path.reverse()
+    return path
+
+
 def dfs(graph, start, end):
     if start == end:
         return [start]
@@ -42,12 +76,12 @@ def find_path(graph, start, through, end):
         return [start]
 
     if start == through and through != end:
-        return dfs(graph, start, end)
+        return bfs(graph, start, end)
     if start != through and through == end:
-        return dfs(graph, start, end)
+        return bfs(graph, start, end)
 
-    path1 = dfs(graph, start, through)
-    path2 = dfs(graph, through, end)
+    path1 = bfs(graph, start, through)
+    path2 = bfs(graph, through, end)
 
     if not path1 or not path2:
         return "Ne postoji put"
@@ -61,16 +95,16 @@ if __name__ == '__main__':
         'A': ['B', 'C'],
         'B': ['A', 'D', 'E', 'G'],
         'C': ['A', 'F'],
-        'D': ['B', 'E'],
+        'D': ['B', 'E', 'I'],
         'E': ['B', 'D'],
         'F': ['C', 'I'],
         'G': ['B', 'H'],
         'H': ['G'],
-        'I': ['F'],
+        'I': ['F', 'D'],
         'J': []
     }
 
-    zadati_graf = {
+    graph2 = {
         'A': ['B', 'C'],
         'B': ['A', 'D', 'E'],
         'C': ['A', 'F', 'G'],
@@ -87,4 +121,12 @@ if __name__ == '__main__':
         'N': ['L']
     }
 
-    print(find_path(zadati_graf, 'A', 'L', 'B'))
+    graph3 = {
+        'A': ['D'],
+        'B': ['C', 'D'],
+        'C': ['B'],
+        'D': ['A', 'B', 'E'],
+        'E': ['D']
+    }
+
+    print(find_path(graph2, 'A', 'N', 'E'))
